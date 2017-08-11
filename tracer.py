0f90be3212b65feb067680883f3da052d724c6ff
DeerMichel <mh74182@gmail.com>
2017-08-11 23:16:18 +0200

    Enhance demo scene

--- tracer.py
+++ tracer.py
@@ -98,21 +98,33 @@ def mix(a, b, mix):
 
 
 light_mat = Material(emission_color=Vector3(3, 3, 3))
-light_sphere = Sphere(Vector3(0, 20, 0), 3)
+light_sphere = Sphere(Vector3(0, 20, -30), 3)
 light_obj = RenderObject(light_sphere, light_mat)
 
 ground_mat = Material(surface_color=Vector3(0.2, 0.2, 0.2))
 ground_sphere = Sphere(Vector3(0, -10004, -20), 10000)
 ground_obj = RenderObject(ground_sphere, ground_mat)
 
-blue_mat = Material(surface_color=Vector3(0, 0, 0.6), transparency=0.1)
-blue_sphere = Sphere(Vector3(0, 0, -20), 4)
-blue_obj = RenderObject(blue_sphere, blue_mat)
+mat1 = Material(surface_color=Vector3(1, 0.32, 0.36), reflectivity=1, transparency=0.5)
+sphere1 = Sphere(Vector3(z=-20), 4)
+obj1 = RenderObject(sphere1, mat1)
+
+mat2 = Material(surface_color=Vector3(0.9, 0.76, 0.46), reflectivity=1)
+sphere2 = Sphere(Vector3(5, -1, -15), 2)
+obj2 = RenderObject(sphere2, mat2)
+
+mat3 = Material(surface_color=Vector3(0.65, 0.77, 0.97), reflectivity=1)
+sphere3 = Sphere(Vector3(5, 0, -25), 3)
+obj3 = RenderObject(sphere3, mat3)
+
+mat4 = Material(surface_color=Vector3(0.9, 0.9, 0.9), reflectivity=1)
+sphere4 = Sphere(Vector3(-5.5, 0, -15), 3)
+obj4 = RenderObject(sphere4, mat4)
 
 scene = []
 scene.append(light_obj)
 scene.append(ground_obj)
-scene.append(blue_obj)
+scene.extend([obj1, obj2, obj3, obj4])
 
 width = 150
 height = 100
